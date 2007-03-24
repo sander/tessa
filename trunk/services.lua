30197f2a3f3be67bac1b00e6ab12e73a1e530101
MWild1 <MWild1@64f6cdd7-342c-0410-b90b-1ffdab3f7f90>
2007-03-24 23:09:10 +0000

    Adding basic events

--- trunk/services.lua
+++ trunk/services.lua
@@ -2,10 +2,12 @@
 services = { };
 
 function services:create(name, func, docstring)
+	assert(type(name) == "string");
 	services[name] = func;
 end
 
 function services:delete(name)
+	assert(type(name) == "string");
 	services[name] = nil;
 end
 
@@ -15,7 +17,7 @@ function services:call(name, ...)
 end
 
 function services:pcall(name, ...)
-	if not services[name] then core:raiserror(string.format("Attempt to call non-existant service %s", name)); return nil; end
+	if type(services[name]) ~= "function" then core:raiserror(string.format("Attempt to call non-existant service %s", name)); return nil; end
 	local res = { pcall(services[name], ...) };
 	if not res[1] then
 		--Error
