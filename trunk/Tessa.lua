da87e7185d7715f1299f6e0acb7c98a51ed82e28
MWild1 <MWild1@64f6cdd7-342c-0410-b90b-1ffdab3f7f90>
2007-04-22 20:31:28 +0000

    Lua support WORKS! :D Next, to polish it off...

---
+++ trunk/Tessa.lua
@@ -0,0 +1,1 @@
+GUI_PostEvent(1, { contact = "me@here", string = "Jeff!!!", status = 0});
--- trunk/src/TessaCore.cpp
+++ trunk/src/TessaCore.cpp
@@ -19,5 +19,7 @@ wxThread::ExitCode TessaCoreThread::Entry()
 
 void TessaCoreThread::PostEvent(int EventID, CoreEventData* Data)
 {
-
+    wxCommandEvent evt(TessaGUIServices::ContactStatusChanged, OurID);
+    evt.SetString(::wxString(Data->string, wxConvUTF8));
+    wxPostEvent(GUIThread, evt);
 }
