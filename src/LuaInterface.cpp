4fe9bfbccd08f41b26b1b1a2f772c645b1c6e61b
MWild1 <MWild1@64f6cdd7-342c-0410-b90b-1ffdab3f7f90>
2007-04-22 23:59:26 +0000

    Forgot to add some files

---
+++ src/LuaInterface.cpp
@@ -0,0 +1,25 @@
+#include "LuaInterface.h"
+
+#include <lua5.1/lua.h>
+#include <lua5.1/lualib.h>
+#include <lua5.1/lauxlib.h>
+
+int lcPostEvent(lua_State* L)
+{
+    LuaInterface* IFObject = (LuaInterface*) lua_touserdata(L, lua_upvalueindex(1));
+    CoreEventData Event;
+    if(!lua_isnumber(L, 1) || !lua_istable(L, 2))
+    {
+        lua_pushboolean(L, false);
+        return 1;
+    }
+    int id = lua_tonumber(L, 1);
+    lua_getfield(L, 2, "contact");
+    Event.contact = lua_tostring(L, -1);
+    lua_getfield(L, 2, "string");
+    Event.string = lua_tostring(L, -1);
+    lua_getfield(L, 2, "status"); // Really should use lua_opt for this
+    Event.status = lua_tonumber(L, -1);
+    IFObject->PostEvent(id, &Event);
+    return 0;
+}
