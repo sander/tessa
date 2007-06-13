5a36e7262ebdd57b3a26728bff5e76a391d19d8d
MWild1 <MWild1@64f6cdd7-342c-0410-b90b-1ffdab3f7f90>
2007-06-13 23:09:23 +0000

    I broke the build :)

--- src/TessaSRMM.cpp
+++ src/TessaSRMM.cpp
@@ -1,10 +1,56 @@
+/*
+static std::map<std::string, TessaSRMM> MessageSessions;
 
-class TessaSRMM : public wxFrame
+class TessaMessageSessionInterface
+{
+    TessaMessageSession(const std::string& Contact, TessaSRMM* HostContainer)
+    {
+        if(HostContainer)
+        {
+            // Create new session inside that container
+        }
+        else
+        {
+            // Create a new container
+        }
+    }
+    ~TessaMessageSession()
+    {
+        // Remove ourselves from container
+    }
+
+    private:
+    std::string m_ContactJID;
+    TessaSRMM* m_Container;
+}
+
+class TessaSRMMContainer : public wxFrame
 {
     TessaSRMM();
     ~TessaSRMM();
 
     private:
-    wxString ContactJID;
+    wxString m_ContactJID;
+    bool m_IsTabbed;
+
+    // Child
 
 };
+
+class TessaMessageInterface
+{
+    private:
+    TessaSRMMContainer* m_ParentContainer;
+
+
+    wxSplitter m_Splitter;
+    wxWindow m_MessageLog;
+    wxWindow m_InputArea;
+}
+
+void TessaSRMM::TessaSRMM(wxWindow *parent, wxWindowId id)
+{
+    Create(parent,id,_T("Messages"),wxDefaultPosition,wxSize(450, 400),wxDEFAULT_FRAME_STYLE,_T("wxFrame"));
+
+}
+ */
